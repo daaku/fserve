0901baae42043ec028c03649a6b156068bbc1595
Naitik Shah <n@daaku.org>
2022-07-14 00:04:14 +0400

    clap to enable customizing addr/dir

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use axum::response::IntoResponse;
 use axum::routing::get_service;
+use clap::Parser;
 use std::io;
 use std::net::SocketAddr;
+use std::path::PathBuf;
 use tower_http::services::ServeDir;
 
 async fn handle_error(err: io::Error) -> impl IntoResponse {
     format!("error: {}", err)
 }
 
+#[derive(Parser, Debug)]
+#[clap(author, version)]
+struct Args {
+    /// Directory to serve files from.
+    #[clap(short, long, default_value = ".")]
+    dir: PathBuf,
+    /// Address to bind to.
+    #[clap(short, long, default_value = "127.0.0.1:8080")]
+    addr: SocketAddr,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), axum::BoxError> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 13892));
-    axum::Server::bind(&addr)
+    let args = Args::parse();
+    axum::Server::bind(&args.addr)
         .serve(
-            get_service(ServeDir::new("."))
+            get_service(ServeDir::new(&args.dir))
                 .handle_error(handle_error)
                 .into_make_service(),
         )
