f86b7b0fbac5a53884877f04f4b7575506af1949
Naitik Shah <n@daaku.org>
2022-07-13 18:55:13 +0400

    serve static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get_service;
+use std::io;
+use std::net::SocketAddr;
+use tower_http::services::ServeDir;
+
+async fn handle_error(err: io::Error) -> impl IntoResponse {
+    eprintln!("error: {}", err);
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), axum::BoxError> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 13892));
+    axum::Server::bind(&addr)
+        .serve(
+            get_service(ServeDir::new("."))
+                .handle_error(handle_error)
+                .into_make_service(),
+        )
+        .await?;
+    Ok(())
 }
