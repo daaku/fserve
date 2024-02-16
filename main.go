ac96cf75225e30bf9d87c75cd22df90701b03ee8
Naitik Shah <n@daaku.org>
2024-02-16 10:43:43 +0530

    print more useful public addresses

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -26,7 +28,31 @@ func main() {
 	dir := flag.String("dir", wd, "directory to serve")
 	addr := flag.String("addr", "0.0.0.0:3000", "address to bind to")
 	flag.Parse()
-	fmt.Printf("Serving %s at http://%s/\n", *dir, *addr)
+
+	// print a useful address
+	p := func(a string) {
+		if !strings.Contains(a, ":") {
+			_, port, _ := net.SplitHostPort(*addr)
+			a = a + ":" + port
+		}
+		fmt.Printf("Serving %s at http://%s/\n", *dir, a)
+	}
+	if !strings.HasPrefix(*addr, "0.") {
+		p(*addr)
+	} else {
+		addrs, err := net.InterfaceAddrs()
+		if err != nil {
+			p(*addr)
+		}
+		for _, address := range addrs {
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					p(ipnet.IP.String())
+				}
+			}
+		}
+	}
+
 	h := withLogging(http.FileServer(http.Dir(*dir)))
 	if err := http.ListenAndServe(*addr, h); err != nil {
 		log.Fatal(err)
